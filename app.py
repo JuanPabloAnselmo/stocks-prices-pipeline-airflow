9c2d8a8b6051d6501da80481b7b9cf23abdd2d5a
Juan_Pablo <jpanselmo@hotmail.com>
2024-09-18 23:46:46 -0300

    Update app.py
    Corrijo estilo PEP8

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from utils.database import create_redshift_engine
 def plot_stock_data(engine: Engine) -> None:
     """
     Fetches stock data from the Redshift database and plots it using Streamlit.
-    
+
     If the data is empty, it will return 0 for all columns.
 
     Args:
@@ -29,7 +29,7 @@ def plot_stock_data(engine: Engine) -> None:
             FROM "2024_juan_pablo_anselmo_schema".daily_stock_prices_table
         """
         df = pd.read_sql_query(query, connection)
-        
+
         # Handle empty dataframe by filling it with zeros
         if df.empty:
             st.write("No data available, returning 0 for all columns.")
@@ -70,14 +70,14 @@ def plot_stock_data(engine: Engine) -> None:
             f"Plot of the variable '{selected_variable}' for symbol '{selected_symbol}'"
         )
 
-        fig, ax = plt.subplots(figsize=(12, 6))  
+        fig, ax = plt.subplots(figsize=(12, 6))
         ax.plot(filtered_df['date'], filtered_df[selected_variable], marker='o')
         ax.set_xlabel('Date')
         ax.set_ylabel(selected_variable)
         ax.set_title(f'Evolution of {selected_variable} for {selected_symbol}')
         ax.grid(True)
 
-        
+
         plt.xticks(rotation=45, ha='right')  
 
         st.pyplot(fig)
